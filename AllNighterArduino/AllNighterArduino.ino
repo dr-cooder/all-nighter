c3c74485c3c9c21d06b679eea55e10f7d6f8c7a9
Felix Warner <fwarner2319@gmail.com>
2022-10-27 03:14:33 -0400

    Dedicated smoothing input to Unity and stubbed out scene ceiling lamp behavior

--- AllNighterArduino/AllNighterArduino.ino
+++ AllNighterArduino/AllNighterArduino.ino
@@ -1,34 +1,17 @@
 const int inputPin = A2;
 const int ledPin = 2;
 
-const int numReadings = 10;
-int readings[numReadings];
-int readIndex = 0;
-int total = 0;
-int average = 0;
-
 int inByte;
 
 void setup() {
+  pinMode(inputPin, INPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  for (int i = 0; i < numReadings; i++) {
-    readings[i] = 0;
-  }
   Serial.begin(9600);
 }
 
 void loop() {
-  total -= readings[readIndex];
-  readings[readIndex] = analogRead(inputPin);
-  total += readings[readIndex];
-  readIndex = readIndex + 1;
-  if (readIndex >= numReadings) {
-    readIndex = 0;
-  }
-  average = total / numReadings;
-
-  Serial.println(average);
+  Serial.println(analogRead(inputPin));
 
   if (Serial.available() > 0) {
     inByte = Serial.read();
--- AllNighterUnity/Assets/Scripts/SceneMgrScript.cs
+++ AllNighterUnity/Assets/Scripts/SceneMgrScript.cs
@@ -5,56 +5,91 @@ using UnityEngine;
 using System.IO.Ports; // For serial
 
 public class SceneMgrScript:MonoBehaviour {
+    [Header("Scene Objects")]
     public Camera mainCamera;
+    public Light ceilingLamp;
+
+    [Header("Arduino I/O")]
     public string sPort = "COM4";
     SerialPort stream; // From the Nano
     public int baud = 9600;
+    public int readingsToSmooth = 144;
 
-    // CHANGE THESE ACCORDING TO THE ROOM'S LIGHT LEVELS
+    [Header("Change according to physical room's light range")]
     public int minLight = 10;
     public int nightUnder = 15;
     public int dayOver = 30;
     public int maxLight = 50;
     public int ledBias = 10;
 
-    public Color dayAmbient;
-    public Color nightAmbient;
+    [Header("Sky and ambient lighting")]
+    public Color daySkyColor;
+    public Color nightSkyColor;
+    [Range(0f, 1f)]
+    public float ambientStrength = 0.5f;
+
+    private int readingCount = 0;
+    private int[] readings;
+    private int readIndex = 0;
+    private int readingTotal = 0;
+    private float readingAvg = 0;
 
     private bool lightsOn = false;
     private bool lightsShouldBeOn = false;
     private bool guyWorking = true;
-    private float guyAnimTime = 0;
+    private float sinceLastAnimFrame = 0;
 
     private Color skyColor = new Color();
 
     void Start()
     {
+        ceilingLamp.gameObject.SetActive(lightsOn);
         stream = new SerialPort(sPort, baud);
         stream.Open(); // Open serial stream
+        int initialReading = int.Parse(stream.ReadLine());
+        readingCount = readingsToSmooth;
+        readings = new int[readingCount];
+        for (int i = 0; i < readingCount; i++)
+        {
+            readings[i] = initialReading;
+        }
+        readingTotal = initialReading * readingCount;
+        readingAvg = initialReading;
     }
 
     void Update()
     {
-        int sunLevel = int.Parse(stream.ReadLine());
-        if (lightsOn) sunLevel -= ledBias;
-        skyColor = Color.Lerp(nightAmbient, dayAmbient, Mathf.InverseLerp(minLight, maxLight, sunLevel));
-        RenderSettings.ambientSkyColor = skyColor;
+        readingTotal -= readings[readIndex];
+        int newReading = int.Parse(stream.ReadLine());
+        if (lightsOn) newReading -= ledBias;
+        readings[readIndex] = newReading;
+        readingTotal += readings[readIndex];
+        readIndex++;
+        if (readIndex >= readingCount)
+        {
+            readIndex = 0;
+        }
+        readingAvg = readingTotal / readingCount;
+
+        skyColor = Color.Lerp(nightSkyColor, daySkyColor, Mathf.InverseLerp(minLight, maxLight, readingAvg));
+        RenderSettings.ambientSkyColor = Color.Lerp(Color.black, skyColor, ambientStrength);
         mainCamera.backgroundColor = skyColor;
+
         if (guyWorking)
         {
-            if (sunLevel < nightUnder && !lightsOn)
+            if (readingAvg < nightUnder && !lightsOn)
             {
                 lightsShouldBeOn = true;
                 guyWorking = false;
-                guyAnimTime = 0;
+                sinceLastAnimFrame = 0;
                 // Play "turning lights on" animation
                 Debug.Log("Night already?!");
             }
-            if (sunLevel > dayOver && lightsOn)
+            if (readingAvg > dayOver && lightsOn)
             {
                 lightsShouldBeOn = false;
                 guyWorking = false;
-                guyAnimTime = 0;
+                sinceLastAnimFrame = 0;
                 // Play "turning lights off" animation
                 Debug.Log("Morning already?!");
             }
@@ -62,15 +97,17 @@ public class SceneMgrScript:MonoBehaviour {
         else
         {
             // Manage turning on/off light animation
-            guyAnimTime += Time.deltaTime;
-            if (guyAnimTime > 1)
+            sinceLastAnimFrame += Time.deltaTime;
+            if (sinceLastAnimFrame > 1)
             {
                 lightsOn = lightsShouldBeOn;
+                ceilingLamp.gameObject.SetActive(lightsOn);
                 Debug.Log($"*Click!* Lights {(lightsOn ? "on" : "off")}!");
                 guyWorking = true;
-                guyAnimTime = 0;
+                sinceLastAnimFrame = 0;
             }
         }
+
         stream.Write(lightsOn ? "1" : "0");
         stream.ReadExisting(); // Dispose extra data
     }
